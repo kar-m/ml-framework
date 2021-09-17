461e8a1cd8391496a5b432d5fc873b632966684f
kar-m <52249278+kar-m@users.noreply.github.com>
2021-09-16 21:23:56 -0700

    Added methods
    The added methods include 
    1. get_leafs - outputs the leafs of a given tree
    2. calculate_gradient_x - calculates the gradient of any given layer, depending on its type (input, hidden or loss)
    3. update_weights - updates the weights of all layers in a given branch given a flattened gradient

--- NeuralNetwork.hpp
+++ NeuralNetwork.hpp
@@ -30,6 +30,8 @@ public:
     string LayerName;
     static LayerNode create_node(Layer* LayerType, int BranchNumber, int TreeDepth, string LayerName);
     static void print_tree(LayerNode root);
+    static LayerNode get_node(LayerNode root, int BranchNumber, int TreeDepth);
+    LayerNode();
     
 private:
     LayerNode(Layer* LayerType, int BranchNumber, int TreeDepth, string LayerName);
@@ -38,13 +40,22 @@ private:
 class NeuralNetwork
 {
 private:
-    NeuralNetwork(vector<queue<LayerNode>> Layers, int batch_size, double learning_rate=0.01, string optimizer="gradient_descent");
+    NeuralNetwork(vector<queue<LayerNode>> Layers, int input_size, int batch_size, double learning_rate=0.01, string optimizer="gradient_descent");
     LayerNode tree_root;
+    vector<LayerNode> tree_leafs;
+    Matrix gradient;
+    Matrix flattened_weights;
+    static void get_leafs(vector<LayerNode> &knownLeafs, LayerNode node);
+    Matrix calculate_gradient_hidden(int BranchNumber, int TreeDepth);
+    Matrix calculate_gradient_loss(int BranchNumber);
+    Matrix calculate_input_loss(int BranchNumber);
+    Matrix get_flattened_weights();
+    void update_weights(Matrix flat_weights);
     
 public:    
-    NeuralNetwork createModel(vector<queue<LayerNode>> Layers, int input_size, int batch_size, double learning_rate=0.01, string optimizer="gradient_descent", string loss="cross_entropy");
+    NeuralNetwork createModel(vector<queue<LayerNode>> Layers, int input_size, int batch_size, double learning_rate=0.01, string optimizer="gradient_descent");
     void fit(Matrix x, Matrix y);
-    void predict(Matrix x);
+    void predict(Matrix x, int branch=0);
     void export_model();
 };
 
